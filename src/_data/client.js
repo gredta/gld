bed61aa27e87c41f72fedf4363bf92bae7e0afac
greta <greta.belegu@gmail.com>
2024-10-04 15:11:15 -0400

    updates

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,20 +1,15 @@
 module.exports = {
     "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "email": "contact@gldplumbing.com",
+    "phoneForTel": "914-200-4009",
+    "phoneFormatted": "(914) 200-4009",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
+        "city": "Westchester",
+        "state": "NY",
         "country": "US",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
     },
     "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
+        "instagram": "https://www.instagram.com/gld_plumbing",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
     "domain": "https://www.example.com"
